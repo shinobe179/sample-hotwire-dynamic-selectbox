8210e30075952266f3bd2b678607820e69e22282
shinobe179 <befs.anne@gmail.com>
2024-04-30 10:02:43 +0900

    依存関係を持ったセレクトボックス実装

--- app/javascript/controllers/country_controller.js
+++ app/javascript/controllers/country_controller.js
@@ -1,7 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
+import { get } from "@rails/request.js"
 
 // Connects to data-controller="country"
 export default class extends Controller {
-  connect() {
+  static targets = ["stateSelect"]
+  change(event) {
+    let country = event.target.selectedOptions[0].value
+    let target  = this.stateSelectTarget.id
+
+    get(`/addresses/states?target=${target}&country=${country}`, {
+      responseKind: "turbo-stream"
+    })
   }
 }
